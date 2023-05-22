c59e8111e9ea489c8c58a01f42ff7e1541c5154b
bainchild <bainchild@letterbomb.ml>
2023-05-22 13:25:54 -0500

    Update envs, init, write_files

--- env_lua5.1.lua
+++ env_lua5.1.lua
@@ -1,1 +1,90 @@
-error('TODO: lua5.1 support');
+local http_request = require "http.request"
+local get_cache = {}
+local CORO_MAIN = newproxy(true)
+local function thread_id()
+   return coroutine.running() or CORO_MAIN
+end
+local function raw_get(uri)
+   --print("rq",uri)
+   local headers, stream = assert(http_request.new_from_uri(uri):go())
+   local body = assert(stream:get_body_as_string())
+   if headers:get ":status" ~= "200" then
+      error(body)
+   end
+   return body
+end
+local function split(a,b)
+   local n = {}
+   for m in (a..b):gmatch("(.-)"..b) do
+      table.insert(n,m)
+   end
+   return n
+end
+function get(url, nocache)
+   if get_cache[url]==nil or os.clock()-get_cache[url][1] > 2 or nocache then
+      get_cache[url] = {os.clock(), raw_get(url)}
+   end
+   return get_cache[url][2]
+end
+local hostname = "http://0.0.0.0:8000"--get("https://gist.githubusercontent.com/bainchild/e7e951e8c5c837acdf2bdb9633859d92/raw")
+function custom_require(name, pre, ...)
+   if type(pre) ~= "string" and type(pre) ~= "number" then pre = "" end
+   local oldsilent = PM.silent
+   if pre == "pm" and false then PM.silent = true end
+   local run = thread_id()
+   local oldpfx = PM.file_prefix[run] or ""
+   if PM.enable_relative_requires then
+      local split_name = split(name,"/")
+      local oldname = name
+      local nd = ((#split_name > 1 and table.concat(split_name, "/", 1, #split_name - 1)) or "")
+      name = ((PM.file_prefix[thread_id()] and PM.file_prefix[thread_id()].."/") or "")..name
+      PM.file_prefix[run] = (oldpfx or "") .. nd
+      -- print(oldname," = change file prefix '",oldpfx,"' -> '",PM.file_prefix[run],"'")
+   end
+   printfm("Requiring /"..pre.."/"..name)
+   local url = hostname.."/"..pre.."/"..name
+   local s, source = pcall(get, url)
+   if not s then s, source = pcall(get, url..".lua") end
+   assert(s, "Error while requesting source: "..tostring(source))
+   if split(source,"\n")[1]:sub(1,2)=="#!" then
+      source = split(source,"\n")
+      table.remove(source,1)
+      source=table.concat(source,"\n")
+   end
+   local res
+   assert(xpcall(function(...)
+      res = {
+         setfenv(assert(loadstring(source, name.." "..(pre and "("..pre..")" or ""))), custom_globals_env)(...);
+      }
+   end, function(...)
+      print("error!", ...);
+      for i = 0, 256 do
+         local r = debug.getinfo(i, "nSl")
+         if r~=nil and #r > 0 then
+            local a = {}
+            for _,v in pairs(r) do table.insert(a,v) end
+            print(i,unpack(a))
+         end
+      end
+      return ...
+   ---@diagnostic disable-next-line: redundant-parameter
+   end, ...))
+   if PM.enable_relative_requires then
+      PM.file_prefix[run] = oldpfx
+   end
+   if pre == "pm" then PM.silent = oldsilent end
+   return unpack(res)
+end
+local old_require = require
+require = function(n, ...)
+   --local old_silent = PM.silent
+   --PM.silent = true
+   local r = {pcall(custom_require, n, "pm", ...)}
+   --PM.silent = old_silent
+   if r[1] then
+      return unpack(r, 2)
+   --else
+   --	print("error when requiring,", unpack(r, 2))
+   end
+   return old_require(n, ...)
+end
--- init.lua
+++ init.lua
@@ -1,79 +1,118 @@
 -- package manager v0.2.5              						                     <noscript>(there would be a docs page here, but javascript is disabled)</noscript><!--
+---@diagnostic disable: undefined-global
 local require, custom_require, PM_G, env = require, nil, nil, nil
-if PM == nil then
-    local setfenv, loadstring, assert, game = setfenv, loadstring, assert, game
-    local printfm = function(...)
-        if PM ~= nil and PM.silent then return end
-        if printf then
-            return printf("<font face=\"Code\">[PM]", ..., "</font>")
-        else
-            return print("[PM]", ...)
-        end
-    end
-    printfm("Initializing...")
-    if game ~= nil and Instance ~= nil and typeof ~= nil and _VERSION == "Luau" then
-        env = "Roblox"
-    else
-        env = (_VERSION:gsub("%s", ""):gsub("%u+",function(n) return n:lower() end))
-        if jit then env = env .. "_jit" end
-    end
-    PM_G = {}
-    local fenv = (function() return _ENV end or getfenv)()
-    local custom_globals_env = setmetatable(PM_G, {__index = fenv})
-    local function hook(a, b) return function(...) return b(a, ...) end end
-    local get
-    !(
-   	local function split(a,b)
-   		local n = {}
-   		for m in (a..b):gmatch("(.-)"..b) do
-   			table.insert(n,m)
-   		end
-   		return n
-   	end
-   	local function popen(cmd)
-   		local file = io.popen(cmd)
-   		local content = file:read("*a")
-   		file:close()
-   		return content
-   	end
-   	local s = "if "
-   	local spl = {}
-   	for i,v in pairs(split(popen("ls -1 pm | grep env"),"\n")) do
-   		if v:sub(1,4)=="env_" then
-   			table.insert(spl,v)
-   		end
-   	end
-   	for i,file in pairs(spl) do
-   		local sorce = loadResource("pm/"..file)
-   		if sorce:sub(#sorce,#sorce)=="\n" then sorce=sorce:sub(1,-2) end
-   		sorce=(sorce:gsub("\n","\n    "))
-   		s=s.."env=='"..file:sub(5,-5).."' then\n    "..sorce..(i~=#spl and "\nelseif " or "")
-   	end
-   	outputLua((s:gsub("\n","\n"..getCurrentIndentationInOutput())))
-    )
-    else
-        error("Unsupported environment: " .. env)
-    end
-    printfm("Initialized.")
-    PM = {
-        conditional_require = require,
-        require = custom_require,
-        _G = PM_G,
-        silent = true,
-        enable_relative_requires = true,
-        file_prefix = {},
-        hostname = hostname,
-        env = env
-    }
-    PM_G.PM = PM
-    pcall(custom_require, "empty", "hooks")
-    PM.silent = false
-else
-    custom_require = PM.require
-    require = PM.conditional_require
-    PM_G = PM._G
-    env = PM.env
+!(
+local function split(a,b)
+   local n = {}
+   for m in (a..b):gmatch("(.-)"..b) do
+      table.insert(n,m)
+   end
+   return n
 end
+local function popen(cmd)
+   local file = assert(io.popen(cmd))
+   local content = file:read("*a")
+   file:close()
+   return content
+end
+local function parseValue(v)
+   if tonumber(v) then return tonumber(v) end
+   if v=="true" or v=="false" then return v=="true" end
+   if v=="nil" then return nil end
+   return v
+end
+local _PARAMS,_RTPARAMS = _G._PARAMS or {},_G._RTPARAMS or {}
+if _G._PARAMS==nil then
+   local realparams,metaparams = {},{}
+   if dataFromCommandLine then
+      local metas,reals = {},{}
+      for m,v in dataFromCommandLine:gmatch("%-[Mm]([^= ]-) ?=? ?(%b\"\")") do
+         metaparams[m]=parseValue(v:sub(2,-2))
+         metas[m]=true
+      end
+      for r,v in dataFromCommandLine:gmatch("%-[Rr]([^= ]-) ?=? ?(%b\"\")") do
+         realparams[r]=parseValue(v:sub(2,-2))
+         reals[r]=true
+      end
+      for m in dataFromCommandLine:gmatch("%-[Mm]([^= ]+)") do
+         if metas[m]==nil then metaparams[m]=true end
+      end
+      for r in dataFromCommandLine:gmatch("%-[Rr]([^= ]+)") do
+         if reals[r]==nil then realparams[r]=true end
+      end
+   end
+   _PARAMS,_RTPARAMS=metaparams,realparams
+   _G._PARAMS,_G._RTPARAMS=_PARAMS,_RTPARAMS
+end
+outputLuaTemplate("local _PARAMS=?\n",_RTPARAMS)
+)
+if PM == nil then
+   local setfenv, loadstring, assert, game = setfenv, loadstring, assert, game
+   local printfm = function(...)
+      if PM ~= nil and PM.silent then return end
+      if printf then
+         return printf("<font face=\"Code\">[PM]", ..., "</font>")
+      elseif package then
+         return print("\27[31m[PM]\27[0m",...)
+      else
+         return print("[PM]", ...)
+      end
+   end
+   printfm("Initializing...")
+   if game ~= nil and Instance ~= nil and typeof ~= nil and _VERSION == "Luau" then
+      env = "Roblox"
+   else
+      env = (_VERSION:gsub("%s", ""):gsub("%u+",function(n) return n:lower() end))
+      if jit then env = env .. "_jit" end
+   end
+   PM_G = {}
+   local fenv = (getfenv or function() return _ENV end)()
+   local custom_globals_env = setmetatable(PM_G, {__index = fenv})
+   local function hook(a, b) return function(...) return b(a, ...) end end
+   local get
+   !(
+   local s = "if "
+   local spl = {}
+   for _,v in pairs(split(popen("ls -1 extra/pm | grep env"),"\n")) do
+      if v:sub(1,4)=="env_" then
+         table.insert(spl,v)
+      end
+   end
+   for i,file in pairs(spl) do
+      local sorce = loadResource("extra/pm/"..file)
+      if sorce:sub(#sorce,#sorce)=="\n" then sorce=sorce:sub(1,-2) end
+      sorce=(sorce:gsub("\n","\n   "))
+      s=s.."env=='"..file:sub(5,-5).."' then\n   "..sorce..(i~=#spl and "\nelseif " or "")
+   end
+   if #spl==0 then s=s.."true then\n   " else s=s.."\nelse" end
+   outputLua((s:gsub("\n","\n"..getCurrentIndentationInOutput())))
+   )
+      error("Unsupported environment: " .. env)
+   end
+   printfm("Initialized.")
+   PM = {
+      conditional_require = require,
+      require = custom_require,
+      _G = PM_G,
+      silent = true,
+      enable_relative_requires = true,
+      file_prefix = {},
+      env = env
+   }
+   PM_G.PM = PM
+   PM_G.custom_require = PM.require
+   PM_G.require = PM.conditional_require
+   PM_G.env = PM.env
+   pcall(custom_require, "empty", "hooks")
+   !(if _PARAMS.EMU then outputLuaTemplate("pcall(custom_require, ?, \"pm\")",_PARAMS.EMU) end)
+   PM.silent = false
+else
+   custom_require = PM.require
+   require = PM.conditional_require
+   PM_G = PM._G
+   env = PM.env
+end
+setfenv(1,PM_G)
 --[[--!>
 <script>
 // TODO: use the preprocess stuff to make the base64 string dynamically
